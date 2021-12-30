3ca803fb972ce7c7be7c7cae4d3e981570ebd41f
ivo-bass <ivailo.ignatoff@gmail.com>
2021-12-30 18:27:53 +0200

    fix read/write all import error

--- grad_task/core/__init__.py
+++ grad_task/core/__init__.py
@@ -1,5 +1,5 @@
-from read_all import read_and_build_dataframes
-from write_all import write_files
+from .read_all import read_and_build_dataframes
+from .write_all import write_files
 
 from .builders.kdf_builder import KdfBuilder
 
